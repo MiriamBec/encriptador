a5998cfa399f4930e37bce2e9b8bae96f68df58d
miribec <miriam8894bec@gmail.com>
2023-06-02 16:28:13 -0700

    Se han creado las funciones de encriptar y desencriptar en javaScript

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-let texto =
-  "Hola mi nombre es miriam, soy estudiante de desarrollo web y estoy tratando de hacer un encriptador";
-
+let texto;
+let buttonEncriptar = document.querySelector("#button-encriptar");
+let buttonDesencriptar = document.querySelector(".button-desencriptar");
+let mensaje = document.querySelector(".mensaje-no-encontrado");
+let mensaje2 = document.querySelector(".mensaje2");
+let copiarButton = document.getElementById("copiar");
 function encriptarTexto() {
-  texto = texto.replace(/a/g, "ai");
+  texto = document.getElementById("myTextarea").value;
   texto = texto.replace(/e/g, "enter");
   texto = texto.replace(/i/g, "imes");
+  texto = texto.replace(/a/g, "ai");
   texto = texto.replace(/o/g, "ober");
   texto = texto.replace(/u/g, "ufat");
-  return texto;
+  mensaje.style.fontWeight = "normal";
+  mensaje2.style.display = "none";
+  copiarButton.style.display = "block";
+  return (mensaje.innerHTML = texto);
 }
 
-let segundoTexto =
-  "Hoberlaimes mimes nobermbrenter enters mimesrimesaimesm, sobery enterstufatdimesaimesntenter denter dentersaimesrroberllober wenterb y enterstobery traimestaimesndober denter haimescenterr ufatn enterncrimesptaimesdoberr";
 function desencriptarTexto() {
-  segundoTexto = segundoTexto.replace(/ai/g, "a");
-  segundoTexto = segundoTexto.replace(/enter/g, "e");
-  segundoTexto = segundoTexto.replace(/imes/g, "i");
-  segundoTexto = segundoTexto.replace(/ober/g, "o");
-  segundoTexto = segundoTexto.replace(/ufat/g, "u");
-  return segundoTexto;
+  texto = document.getElementById("myTextarea").value;
+  texto = texto.replace(/enter/g, "e");
+  texto = texto.replace(/imes/g, "i");
+  texto = texto.replace(/ai/g, "a");
+  texto = texto.replace(/ober/g, "o");
+  texto = texto.replace(/ufat/g, "u");
+  mensaje.style.fontWeight = "normal";
+  mensaje2.style.display = "none";
+  copiarButton.style.display = "block";
+  return (mensaje.innerHTML = texto);
 }
 
-console.log(encriptarTexto());
-console.log(desencriptarTexto());
+buttonEncriptar.onclick = encriptarTexto;
+buttonDesencriptar.onclick = desencriptarTexto;
+// console.log(encriptarTexto());
+// console.log(desencriptarTexto());
